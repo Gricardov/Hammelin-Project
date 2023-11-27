3a01cb302045b9b2e123cdbc712eacc6a14482aa
gricardov <gricardov@gmail.com>
2023-11-27 11:33:31 -0500

    done

--- hammelin-node/controller/reports.js
+++ hammelin-node/controller/reports.js
@@ -5,6 +5,7 @@ const registerReport = async (req, res) => {
   try {
     const { idObra, nombres, apellidos, telefono, correo, observacion, satisfaccionPorc, docsJson } = req.body;
     await queryDB('CALL registrar_reporte(?,?,?,?,?,?,?,?)', [idObra, nombres, apellidos, telefono, correo, observacion, satisfaccionPorc, JSON.stringify(docsJson)]);
+    res.setHeader("Content-Type", "application/json");
     res.json({ message: 'Reporte registrado exitosamente' });
   } catch (error) {
     console.log('No se pudo registrar el reporte: ', error);
